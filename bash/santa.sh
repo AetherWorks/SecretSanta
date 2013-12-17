76a867cb924e2cca66b5b182dcb5bbd3879851a3
gbigwood <gbigwood@aetherworks.com>
2013-12-17 09:11:16 -0500

    Add bash secret santa
    Add email addresses to emails.txt
    Run with ./santa.sh
    Tested on bash 4.2

---
+++ bash/santa.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+actionSanta ()
+{ # what should we do with our santa list?
+	echo $1 "buys for" $2
+	#If you want to mail rather than displaying:
+	echo "Hi $1, you buy for $2" | mail -s "Secret Santa " $1
+} 
+
+#Shuffle and write to new file
+shuf emails.txt > /tmp/shuffnames.txt
+
+#Read into memory
+readarray shuffednames < /tmp/shuffnames.txt
+
+firstPerson=${shuffednames[0]}
+count=0
+for i in "${shuffednames[@]}"
+do
+	if [ $count -gt 0 ]; then
+		#echo $i"buys for" $prevName
+		actionSanta $i $prevName
+	fi
+	prevName=$i
+	count+=1
+done
+
+actionSanta $firstPerson $i
