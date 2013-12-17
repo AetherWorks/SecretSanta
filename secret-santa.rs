f75078c155ab9e19e96cbb4d91caa136ad875451
lewisheadden <lh439@st-andrews.ac.uk>
2013-12-17 00:32:31 -0500

    Fixed passing of vector

--- secret-santa.rs
+++ secret-santa.rs
@@ -6,10 +6,12 @@ fn main() {
 
 	let names = ~["Angus", "Greg", "Lewis", "Mike", "Isabel", "Rob", "Shannon", "Allan"];
 
-	secret_santa(names);	
+	let pairings = secret_santa(&names);	
+
+	print_pairings(&pairings);
 }
 
-fn secret_santa(names: ~[&str]) {
+fn secret_santa<'r>(names: &~[&'r str]) -> ~[(&'r str,&'r str)] {
 	let mut rng = rand::rng();
 	let mut unshuffled_names = names.clone();
 	let mut shuffled_names = rng.shuffle(names.clone());
@@ -38,13 +40,13 @@ fn secret_santa(names: ~[&str]) {
 
 	if(shuffled_names.len() == 1) {
 		println("Restarting - no good solution.");
-		secret_santa(names.clone());
-	} else {
-		print_pairings(pairs);
+		secret_santa(names);
 	}
+
+	return pairs.clone();
 } 
 
-fn print_pairings(pairings: ~[(&str, &str)]) {
+fn print_pairings(pairings: &~[(&str, &str)]) {
 	for &pair in pairings.iter() {
 			println!("{:?} --- is buying for ---> {:?}", pair.first(), pair.second());
 	}
