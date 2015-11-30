ea20a7e18edb7152f93ae9a0792dc59441d9ea13
Pete Savitsky <petesavitsky@users.noreply.github.com>
2015-11-30 10:39:01 -0500

    Add comments and clean up

--- go/secretsanta/main.go
+++ go/secretsanta/main.go
@@ -14,21 +14,26 @@ func main() {
 		"Rob", "Renee", "Shannon", "Greg"}
 	permutations := make([]Pair, 0)
 	shuffledPermutations := make([]Pair, 0)
+
+	// create all permutations
 	for i := 0; i < len(names); i++ {
 		for j := i + 1; j < len(names); j++ {
 			forwardPair := Pair{santa: names[i], receiver: names[j]}
-			permutations = append(permutations, forwardPair)
 			backwardPair := Pair{santa: names[len(names)-(i+1)],
 				receiver: names[len(names)-(j+1)]}
-			permutations = append(permutations, backwardPair)
+			permutations = append(permutations, forwardPair, backwardPair)
 		}
 	}
+
+	// randomly shuffle list of permutations
 	rand.Seed(time.Now().UnixNano())
 	for len(permutations) > 0 {
 		random := rand.Intn(len(permutations))
 		shuffledPermutations = append(shuffledPermutations, permutations[random])
 		permutations = append(permutations[:random], permutations[random+1:]...)
 	}
+
+	// iterate over permutations until full
 	for _, pair := range shuffledPermutations {
 		if len(santaList) == 2*len(names) {
 			break
@@ -37,11 +42,14 @@ func main() {
 			santaList = append(santaList, pair)
 		}
 	}
+
+	// print out results
 	for _, pair := range santaList {
 		log.Printf("%s --> %s\n", pair.santa, pair.receiver)
 	}
 }
 
+// determine whether or not a giver or receiver in this pair has already been used
 func isValidPair(pair Pair) bool {
 	for _, santaPair := range santaList {
 		if santaPair.santa == pair.santa ||
